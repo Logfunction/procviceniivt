4dd5aa93eb4030286e4c82c9b2ac3f01db19c151
Logfunction <152821282+Logfunction@users.noreply.github.com>
2024-02-27 10:31:38 +0100

    Add files via upload

---
+++ procvicovani_ukol/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.Design;
+
+Console.WriteLine("zadej dvojici čísel a nuly");
+int x;
+int y;
+int max = 0;
+x = Convert.ToInt32(Console.ReadLine());
+y = Convert.ToInt32(Console.ReadLine());
+while (x!=0 && y != 0)
+{
+    if (x + y > max)
+    {
+        max = x + y;
+    }
+
+    x = Convert.ToInt32(Console.ReadLine());
+    y = Convert.ToInt32(Console.ReadLine());
+
+}
+
+Random random = new Random();
+int r = random.Next(0, 100);
+
+if (r > max)
+{
+    Console.WriteLine("součet je menší, než náhodné číslo");
+}
+else
+{
+    Console.WriteLine("součet je menší, než náhodné číslo");
+}
+
+Console.WriteLine("nejvetsi soucet je " + max);
